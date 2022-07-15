4ea72ef02167791b7ea5fff5ffdcaa557cad91f4
Dylan Stark <dylan.stark@gmail.com>
2022-07-15 10:42:06 -0500

    Refactors logging function; removes decorator.

--- optuna/integration/comet.py
+++ optuna/integration/comet.py
@@ -6,28 +6,32 @@ with try_import() as _imports:
     from comet_ml import Experiment
 
 
-@experimental_class("2.9.0")
 class CometCallback:
     """A callback to for logging study trials in Comet."""
 
     def __call__(self, study, trial) -> None:
         """Logs study and trial info to Comet if it is installed."""
-
         if _imports.is_successful():
-            experiment = Experiment(project_name=study.study_name)
-            experiment.set_name(f"trail_{trial.number}")
+            log_study_trial(study, trial)
 
-            experiment.log_parameters(trial.params)
-            experiment.log_metrics(
-                dic={
-                    "value": trial.value,
-                },
-                step=0)
-            experiment.log_others({
-                "trial_number": trial.number,
-                "trial_datetime_start": trial.datetime_start,
-                "trial_system_attrs": trial.system_attrs,
-                "trial_user_attrs": trial.user_attrs,
-                "study_direction": study.direction,
-            })
-            experiment.end()
+
+def log_study_trial(study, trial) -> None:
+    """Logs a single trial for the given study to Comet."""
+    project_name = f"sklearn_simple_comet_sdk_{study.study_name}"
+    experiment = Experiment(project_name=project_name)
+    experiment.set_name(f"trail_{trial.number}")
+
+    experiment.log_parameters(trial.params)
+    experiment.log_metrics(
+        dic={
+            "value": trial.value,
+        },
+        step=0)
+    experiment.log_others({
+        "trial_number": trial.number,
+        "trial_datetime_start": trial.datetime_start,
+        "trial_system_attrs": trial.system_attrs,
+        "trial_user_attrs": trial.user_attrs,
+        "study_direction": study.direction,
+    })
+    experiment.end()
